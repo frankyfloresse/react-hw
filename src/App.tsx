530f68b5f6c3b2a905c1142fbba39f3defd2a788
frankyfloresse <foz.fox2015@gmail.com>
2025-01-14 16:51:01 +0200

    add lesson 5/task 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import './App.css'
+import {FormEvent, useState} from "react";
+
+interface IFormProps {
+    username: string;
+}
 
 function App() {
+    const [form, setForm] = useState<IFormProps>({
+        username: '',
+    })
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        let user = {
+            username: form.username,
+        }
+
+        console.log(user);
+    }
+
+    const handleInput = (e: FormEvent<HTMLInputElement>) => {
+        const input = e.target as HTMLInputElement;
+        setForm({...form, [input.name]: input.value})
+    }
+
 
   return (
       <>
+          <form onSubmit={handleSubmit}>
+              <input type="text" name={"username"} value={form.username} onChange={handleInput} />
+          </form>
       </>
   )
 }
